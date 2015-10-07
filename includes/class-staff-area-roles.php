a549aa044938d731778ac1ea27950198b987cfe3
David Egan <david@carawebs.com>
2015-10-07 18:26:26 +0100

    Add readme

---
+++ includes/class-staff-area-roles.php
@@ -0,0 +1,45 @@
+<?php
+namespace Carawebs\Staff;
+/**
+ * The file that defines the Loops class
+ *
+ *
+ * @link       http://carawebs.com/plugins/staff-area
+ * @since      1.0.0
+ *
+ * @package    Staff_Area
+ * @subpackage Staff_Area/includes
+ */
+
+/**
+ * The loops class - used to define custom loops.
+ *
+ * Passing an array of arguments when instantiating the class object allows the
+ * default $args to be overridden.
+ *
+ * @since      1.0.0
+ * @package    Staff_Area
+ * @subpackage Staff_Area/includes
+ * @author     David Egan <david@carawebs.com>
+ */
+class Roles {
+
+  /**
+   * Arguments to be passed to WP_Query()
+   * @since    1.0.0
+   * @var array
+   */
+  private $args;
+
+  /**
+   * Set up the default arguments for XXXX.
+   *
+   *
+   * @since    1.0.0
+   * @param array $override Array of WP_Query arguments
+   */
+  public function __construct( ) {
+
+  }
+
+}
---
+++ readme.md
@@ -0,0 +1,11 @@
+Staff Area
+==========
+
+Creates a staff area on a page called 'staff'.
+
+Registers 'staff_resource' custom post type, with a custom taxonomy 'resource_category'.
+
+Registers custom user roles:
+
+* 'staff_member' an ordinary staff member
+* 'staff_manager' staff member with management privileges
