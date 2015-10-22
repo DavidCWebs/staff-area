56a474af610024cafe22a0005bfe34aa21408562
David Egan <david@carawebs.com>
2015-10-22 21:51:27 +0100

    Minor display amendments

--- templates/partials/staff-member.php
+++ templates/partials/staff-member.php
@@ -5,6 +5,7 @@
 $user = new Staff_Area\Members\User_Data( $staff_member_ID );
 $userdata = $user->get_userdata();
 ?>
+<p><a href="<?php echo esc_url( home_url('/staff-management') ); ?>">&laquo;&nbsp;Back to Staff Management</a></p>
 <h3>This is the Staff Record for: <?php echo $userdata['full_name']; ?></h3>
 
 <table class="table">
@@ -27,16 +28,32 @@ $userdata = $user->get_userdata();
     </tr>
   </tbody>
 </table>
+<h3>Staff Resources</h3>
+<?php if ( is_array( $userdata['completed'] ) ){
 
-<h3>Completed Resources</h3>
-<table class="table">
-  <thead>
-    <tr>
-      <th>Title</th>
-      <th>Date Completed</th>
-    </tr>
-  </thead>
-  <tbody>
-    <?php echo $user->get_completed_resources( 'rows' ); ?>
-  </tbody>
-</table>
+  ?>
+  <p>
+    <?php echo $userdata['first_name']; ?> has marked the following staff resources as complete:
+  </p>
+  <table class="table">
+    <thead>
+      <tr>
+        <th>Title</th>
+        <th>Date Completed</th>
+      </tr>
+    </thead>
+    <tbody>
+      <?php echo $user->get_completed_resources( 'rows' ); ?>
+    </tbody>
+  </table>
+<?php
+
+} else {
+
+  ?>
+  <p>
+    <?php echo $userdata['first_name']; ?> has not marked any staff resources as complete.
+  </p>
+  <?php
+
+}
