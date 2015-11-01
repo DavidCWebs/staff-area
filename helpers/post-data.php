8a42313849f07b99a289d48619fe25a14a357c1a
David Egan <david@carawebs.com>
2015-11-01 17:47:41 +0000

    Allow post IDs helper function to be customised

--- helpers/post-data.php
+++ helpers/post-data.php
@@ -4,24 +4,30 @@ namespace Staff_Area\Helpers;
 class Post_Data {
 
 /**
-  * Return an array of post IDs
+  * Return an array of post IDs for the given CPT
   *
-  * Build a more efficient query by only returning an array of post IDs.
+  * Builds an efficient query by only returning an array of post IDs.
+  * By default, returns an array of IDs for all published posts of the custom
+  * post type specified by the first argument ($cpt). The second argument allows
+  * accepts an array that allows the default WP_Query() arguments to be overridden.
   *
   * @author David Egan
   * @see http://codex.wordpress.org/Class_Reference/WP_Query#Return_Fields_Parameter
   * @param  string $cpt The custom post type to fetch
   * @return array Array of post IDs
   */
-  public static function custom_post_type_IDs( $cpt ) {
+  public static function custom_post_type_IDs( $cpt, $override = '' ) {
 
-    $args = array(
+    $override = empty( $override ) ? [] : $override;
+
+    $args = array_merge( array(
       'post_type'     => $cpt,
       'post_status'   => 'publish',
       'fields'        => 'ids',
+      'posts_per_page'  => '-1',
+      ), $override
     );
 
-    // The Query
     $result_query = new \WP_Query( $args );
 
     $ID_array = $result_query->posts;
