c4f7c42bda8043968593e9f10115f101085c4f8e
David Egan <david@carawebs.com>
2015-12-02 17:05:46 +0000

    Fix permalink to associated resources on staff page

--- templates/partials/associated-resource-list.php
+++ templates/partials/associated-resource-list.php
@@ -5,6 +5,6 @@ $resources = Staff_Area\Helpers\Post_Data::resources_linked_to_business_unit( $b
 ?>
 <ul>
   <?php foreach( $resources as $resource ) : ?>
-  <li><a href="<?= get_the_permalink; ?>"><?= get_the_title( $resource );?></a></li>
+  <li><a href="<?= get_the_permalink( $resource ); ?>"><?= get_the_title( $resource );?></a></li>
 <?php endforeach; ?>
 </ul>
